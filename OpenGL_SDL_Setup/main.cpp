ee3b5ac2caa9a32b81c4ca56f3fa8edc9d2e1c72
souvik1914581 <souvikroyofficial10@gmail.com>
2022-12-15 21:57:55 +0530

    Removing old main file

--- OpenGL_SDL_Setup/main.cpp
+++ OpenGL_SDL_Setup/main.cpp
@@ -1,7 +1,4 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2022)
-and may not be redistributed without written permission.*/
 
-//Using SDL, SDL OpenGL, GLEW, standard IO, and strings
 #include <SDL.h>
 #include <glad/glad.h>
 #include <SDL_opengl.h>
